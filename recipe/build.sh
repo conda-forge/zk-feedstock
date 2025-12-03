668d905483c7ceb4de4996fbb3b32fd0e443388f
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-03 15:10:48 -0800

    use rattler

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" -tags fts5,icu
+go build -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w" -tags fts5,icu
 go-licenses save . --save_path=license-files --ignore github.com/zk-org/zk
